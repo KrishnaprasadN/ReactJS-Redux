8e7b30d36bdd399c7536fc82cc0a8b76fec67aef
Krishnaprasad <krishnaprasadnaravi@gmail.com>
2021-06-28 21:28:03 +0530

    Implemented the Redux concept in Class Component, which uses connect() method with subscriber and dispatch functions.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,8 @@
 import classes from './Counter.module.css';
-import { useSelector, useDispatch } from 'react-redux'
+import {Component} from 'react'
+import { useSelector, useDispatch, connect } from 'react-redux'
 
-const Counter = () => {
+/* const Counter = () => {
   // dispatches change events
   const dispatch = useDispatch()
 
@@ -20,7 +21,7 @@ const Counter = () => {
 
   return (
     <main className={classes.counter}>
-      <h1>Redux Counter</h1>
+      <h1>Redux Counter with Functional Component</h1>
       <div className={classes.value}>{counter}</div>
 
       <div>
@@ -31,7 +32,53 @@ const Counter = () => {
 
 
     </main>
-  );
-};
+  )
+}
 
-export default Counter;
+export default Counter */
+
+class Counter extends Component {
+
+  incrementCounterHandler() {
+    this.props.increment()
+  }
+
+  decrementCounterHandler() {
+    this.props.decrement()
+  }
+
+  render() {
+    return(
+    <main className={classes.counter}>
+      <h1>Redux Counter with Class Component</h1>
+      <div className={classes.value}>{this.props.counter}</div>
+
+      <div>
+        <button onClick={this.incrementCounterHandler.bind(this)}>Increment Counter</button>
+        <p></p>
+        <button onClick={this.decrementCounterHandler.bind(this)}>Decrement Counter</button>
+      </div>
+    </main>
+    ) 
+
+  }
+}
+
+// subscriber equivalent to useSelector() hook
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter
+  }
+}
+
+// dispatcher equivalent to useDispatch() hook
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => {dispatch({ type: 'IN', payload: 2 })},
+    decrement: () => {dispatch({ type: 'DE', payload: 1 })}
+  }
+
+}
+
+// connect is used to connect the Class Component to the Redux Store
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
